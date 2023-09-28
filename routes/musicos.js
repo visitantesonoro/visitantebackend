97be87d26de6a864cdd2fe141250cd53fe8a5b3d
alejoforeroforero <alejoforeroforero@gmail.com>
2023-09-28 08:23:53 -0500

    external upload

--- routes/musicos.js
+++ routes/musicos.js
@@ -7,6 +7,7 @@ const multer = require("multer");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
 const { checkAuth } = require("../middleware/check-auth");
+var FTPStorage = require("multer-ftp");
 
 router.get("/", musicosController.traerMusicos);
 router.get("/:id", musicosController.traerMusico);
@@ -18,30 +19,60 @@ const MIME_TYPE_MAP = {
   "image/jpg": "jpg",
 };
 
+// const fileUpload = multer({
+//   limits: 500000,
+//   storage: multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, "uploads/images");
+//     },
+//     filename: (req, file, cb) => {
+//       const ext = MIME_TYPE_MAP[file.mimetype];
+//       const id = crypto.randomUUID();
+//       const lastIndex = file.originalname.lastIndexOf(".");
+//       const fileN = file.originalname.slice(0, lastIndex);
+//       const name = `${fileN}-${id}.${ext}`;
+     
+//       cb(null, name);
+//     },
+//   }),
+//   fileFilter: (req, file, cb) => {
+//     const isValid = !!MIME_TYPE_MAP[file.mimetype];
+//     let error = isValid ? null : "invalido";
+//     cb(error, isValid);
+//   },
+// });
+
 const fileUpload = multer({
   limits: 500000,
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "uploads/images");
-    },
-    filename: (req, file, cb) => {
+  storage: new FTPStorage({
+    destination: function (req, file, options, callback) {
+
       const ext = MIME_TYPE_MAP[file.mimetype];
       const id = crypto.randomUUID();
       const lastIndex = file.originalname.lastIndexOf(".");
       const fileN = file.originalname.slice(0, lastIndex);
-      const name = `${fileN}-${id}.${ext}`;
-     
-      cb(null, name);
+
+      const name = `./uploads/admin/perfiles/${fileN}-${id}.${ext}`;
+
+      callback(null, name);
+    },
+    ftp: {
+      host: process.env.ALEJO_HOST,
+      secure: false, // enables FTPS/FTP with TLS
+      user: process.env.ALEJO_USER,
+      password: process.env.ALEJO_PASSWORD
     },
   }),
   fileFilter: (req, file, cb) => {
     const isValid = !!MIME_TYPE_MAP[file.mimetype];
+
     let error = isValid ? null : "invalido";
+
     cb(error, isValid);
   },
-});
+})
 
-//router.use(checkAuth);
+router.use(checkAuth);
 
 router.post(
   "/crear",
